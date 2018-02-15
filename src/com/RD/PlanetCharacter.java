8468ad1f8f8a9c328ecbd002298463c322a66bfc
elgraney <elgraney501@gmail.com>
2018-02-15 14:53:04 +0000

    Minor updates

--- src/com/RD/PlanetCharacter.java
+++ src/com/RD/PlanetCharacter.java
@@ -14,8 +14,12 @@ public class PlanetCharacter {
     private int surfaceTemperature;
     public PlanetCharacter(int distance, char sunType){
 
-        surfaceTemperature = calcSurfaceTemp(distance, sunType);
-
+        //1. temp
+        surfaceTemperature = (int) calcSurfaceTemp(distance, sunType);
+        //2. planet size
+        //3. planet type
+        //4. planet colour
+        //5. planet POIs
         this.skin = chooseSkin(distance, surfaceTemperature);
         try {
             ImageIO.write(this.skin, "png", new File("colour test.png"));
@@ -27,52 +31,53 @@ public class PlanetCharacter {
     private BufferedImage chooseSkin(int distance, int surfaceTemperature){
         //determine canvas to use
         //not testable yet so just import default
+        BufferedImage newSkin=null;
         try {
-            BufferedImage img = ImageIO.read(new File("Test canvas.png"));
-            BufferedImage skin =PlanetCarver.carve(img ,500);
-            skin = setColour(skin);
+            BufferedImage img = ImageIO.read(new File("Continents V1 small.png"));
+            newSkin =PlanetCarver.carve(img ,500);
+            newSkin = setColour(newSkin);
         }
         catch (IOException e) {
             e.printStackTrace();
         }
 
-        return skin;
+        return newSkin;
     }
 
-    private int calcSurfaceTemp(int distance, char sunType){
+    private double calcSurfaceTemp(int distance, char sunType){
         int sunMod = 1;
         switch (sunType){
             case 's':
-                sunMod =1;
+                sunMod =150;
                 break;
             case 'o':
-                sunMod =1;
+                sunMod =120;
                 break;
             case 'b':
-                sunMod =1;
+                sunMod =100;
                 break;
             case 'a':
-                sunMod =1;
+                sunMod =90;
                 break;
             case 'f':
-                sunMod =1;
+                sunMod =80;
                 break;
             case 'g':
-                sunMod =1;
+                sunMod =72;
                 break;
             case 'k':
-                sunMod =1;
+                sunMod =65;
                 break;
             case 'm':
-                sunMod =1;
+                sunMod =60;
                 break;
             case 'd':
-                sunMod =1;
+                sunMod =20;
                 break;
         }
 
 
-        int temp = (1/(distance*distance))*sunMod;
+        double temp = -sunMod*Math.log(distance/ 10000000);
         return temp;
     }
 
@@ -89,6 +94,16 @@ public class PlanetCharacter {
 
         from = Color.decode("#7F7F7F"); //grey
         to = new Color(9843455, false);
+        lookup = new LookupOp(new ColourMapper(from, to), null);
+        convertedImage = lookup.filter(convertedImage, null);
+
+        from = Color.decode("#FF0000"); //red
+        to = new Color(0xFF00DC, false);
+        lookup = new LookupOp(new ColourMapper(from, to), null);
+        convertedImage = lookup.filter(convertedImage, null);
+
+        from = Color.decode("#00FF00"); //green
+        to = new Color(0xFF8800, false);
         lookup = new LookupOp(new ColourMapper(from, to), null);
         convertedImage = lookup.filter(convertedImage, null);
 
